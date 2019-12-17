02afd6b0d5b39cc79eb1736bf903adf9e33201fb
William Mechler <31900802+hackwill@users.noreply.github.com>
2019-12-17 15:06:18 -0500

    Add files via upload

---
+++ Week1/Test1.java
@@ -0,0 +1,14 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Test1 {
+  
+  
+  public static void main(String[] args) { 
+    
+    System.out.println("\nHello Everyone");
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
---
+++ Week1/msp.java
@@ -0,0 +1,19 @@
+/**
+ * Name: William Mechler
+ * Course: CSE 174 MA
+ * Instructor: Dave Woods
+ * Lab 1 - Hello World Program
+ * Purpose: Display a simple message
+ */
+
+class msp { 
+  
+  public static void main(String args[]) {
+    
+    /* Simple output text to console*/
+    
+    System.out.println("Hello testing 1 2 3");
+    
+  } 
+  
+}
